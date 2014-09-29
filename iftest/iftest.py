0047412bf6991e0d3bbbe30ed63dd4439a8119b8
qinxingxing <qinxingxing@vivo.com.cn>
2014-09-29 20:24:55 +0800

    Add two files

---
+++ iftest/iftest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# iftest.py
+
+#age = 20
+#if age >= 18:
+#	print 'your age is ' , age
+#	print 'adult'
+	
+age = 10
+if age >= 18:
+	print 'your age is ', age 
+	print 'adult'
+elif age >= 6:
+	print 'your age is ', age 
+	print 'teenager'
+else:
+	print 'your age is ', age
+	print 'kid'
